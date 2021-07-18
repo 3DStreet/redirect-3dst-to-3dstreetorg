b18754611162259c3690f67af196b5b971e3ca94
Kieran Farr <kieran.farr@gmail.com>
2021-07-17 20:39:42 -0700

    change to app.3d.st

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,5 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.streetmixRedirect = functions.https.onRequest((req, res) => {
-  res.redirect("https://kfarr.github.io/3dstreet/#https://streetmix.net" + req.path);
+  res.redirect("https://app.3d.st/#https://streetmix.net" + req.path);
 });
