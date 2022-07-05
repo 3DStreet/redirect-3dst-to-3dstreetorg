dd4b379a0270e2f0cd90fecfdf710be51bbfa55a
Kieran Farr <kieran.farr@gmail.com>
2022-07-05 14:18:22 -0700

    v1 attempt

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,6 @@ const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.streetmixRedirect = functions.https.onRequest((req, res) => {
-  res.redirect("https://github.3dstreet.org/#https://streetmix.net" + req.path);
+exports.redirect3dst = functions.https.onRequest((req, res) => {
+  res.redirect("https://3dstreet.org" + req.path);
 });
